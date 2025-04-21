2cc955c049b3e24ae730260a3cabf31159ced6f7
quanrong88 <quanrong1988@yahoo.com.vn>
2025-04-21 15:58:55 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SideMenu",
+    platforms: [
+        .iOS(.v11) 
+    ],
     products: [
         .library(name: "SideMenu",  targets: ["SideMenu"])
     ],
