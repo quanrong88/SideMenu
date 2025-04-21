e4a3fbd5717f54fe95b4cfc4a3a68e53e62dcfa5
quanrong88 <quanrong1988@yahoo.com.vn>
2025-04-21 16:11:35 +0700

    Update SideMenuManager.swift

--- Pod/Classes/SideMenuManager.swift
+++ Pod/Classes/SideMenuManager.swift
@@ -15,6 +15,8 @@
      SideMenuManager.menuAddPanGestureToPresent(toView: self.navigationController!.navigationBar)
      SideMenuManager.menuAddScreenEdgePanGesturesToPresent(toView: self.navigationController!.view)
 */
+import Foundation
+import UIKit
 
 @objcMembers
 open class SideMenuManager : NSObject {
